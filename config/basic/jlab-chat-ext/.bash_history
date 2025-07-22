c361332d972fcc63fdc24d1d8f63bab94a2c5283
rcmurray <>
2025-07-22 13:50:14 -0400

    First valid simple extension

---
+++ config/basic/jlab-chat-ext/.bash_history
@@ -0,0 +1,26 @@
+pwd
+jupyter lab --version
+pip install copier
+pip install jinja2_time
+copier copy https://github.com/jupyterlab/extension-template . --trust
+jupyter labextension list
+pwd
+ls -al
+history
+jlpm install
+ls -al
+exit
+ps -aux | grep jupyter
+exit
+jupyter labextension list
+copier --version
+jupyter lab --version
+jlpm install
+exit
+jupyter lab --version
+pip install .
+jupyter labextension list
+exit 
+pip install .
+jupyter labextension list
+exit
---
+++ config/basic/jlab-chat-ext/src/index.ts
@@ -0,0 +1,32 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+/**
+ * Initialization data for the jlab-chat-ext extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jlab-chat-ext:plugin',
+  description: 'JupyterLab extension for chat',
+  autoStart: true,
+  optional: [ISettingRegistry],
+  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+    console.log('JupyterLab extension jlab-chat-ext is activated!');
+
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('jlab-chat-ext settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for jlab-chat-ext.', reason);
+        });
+    }
+  }
+};
+
+export default plugin;
