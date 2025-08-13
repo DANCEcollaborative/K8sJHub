80c2f0275299028a2143037e25b047f415c7b674
rcmurray <>
2025-08-13 12:19:44 -0400

    Checkpoint: No errors & extension loads, but not passing URL

--- config/basic/jlab-chat-ext/serverextension/handlers.py
+++ config/basic/jlab-chat-ext/serverextension/handlers.py
@@ -1,17 +1,22 @@
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import os
-import tornado
+import json
+# from notebook.base.handlers import APIHandler
+import tornado.web
 
 class ChatWSUrlHandler(APIHandler):
     @tornado.web.authenticated
     def get(self):
         chat_url = os.environ.get("CHAT_WS_URL", "")
-        self.finish({"chatUrl": chat_url})
+#         self.finish({"chatUrl": chat_url})
+        self.finish(json.dumps({"chatUrl": chat_url}))
 
 def setup_handlers(web_app):
     host_pattern = ".*$"
-    base_url = web_app.settings["base_url"]
-    route_pattern = url_path_join(base_url, "chat-ext", "wsurl")
-    handlers = [(route_pattern, ChatWSUrlHandler)]
-    web_app.add_handlers(host_pattern, handlers)
+#     base_url = web_app.settings["base_url"]
+#     route_pattern = url_path_join(base_url, "chat-ext", "wsurl")
+#     handlers = [(route_pattern, ChatWSUrlHandler)]
+#     web_app.add_handlers(host_pattern, handlers)
+    route_pattern = "/chat-ext/wsurl"
+    web_app.add_handlers(host_pattern, [(route_pattern, ChatWSUrlHandler)])
